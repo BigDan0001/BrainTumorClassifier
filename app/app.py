1f98bda724a03e4a786267b96fcc9fe3bcdfe095
BigDan0001 <danielpoju03@gmail.com>
2026-05-30 02:53:21 +0300

    Prepare Hugging Face deployment

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-import streamlit as st
+""" import streamlit as st
 import tensorflow as tf
 import numpy as np
 from tensorflow.keras.preprocessing import image
@@ -36,4 +36,28 @@ if uploaded_file is not None:
     st.write(f"**Confidence:** {confidence:.2f}%")
 
     # simple confidence bar
-    st.progress(float(confidence / 100))
+    st.progress(float(confidence / 100)) """ 
+
+import streamlit as st
+import tensorflow as tf
+import numpy as np
+from tensorflow.keras.preprocessing import image
+
+st.title("🧠 Brain Tumor Classifier")
+
+model = tf.keras.models.load_model("model/brain_tumor_model.keras")
+
+class_names = ['glioma', 'meningioma', 'notumor', 'pituitary']
+
+uploaded_file = st.file_uploader("Upload MRI Image")
+
+if uploaded_file is not None:
+    img = image.load_img(uploaded_file, target_size=(224, 224))
+    img_array = image.img_to_array(img)
+    img_array = img_array / 255.0
+    img_array = np.expand_dims(img_array, axis=0)
+
+    prediction = model.predict(img_array)
+
+    st.write("Prediction:", class_names[np.argmax(prediction)])
+    st.write("Confidence:", np.max(prediction) * 100)
